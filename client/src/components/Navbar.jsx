a190c7076ebf0c72eaabd03b269fbdb3973701fc
ujjalzaman <ujjalz@thegates.biz>
2022-11-06 16:15:13 +0600

    Implemented Navbar style

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,8 +1,31 @@
 import React from 'react'
-
+import {Link} from 'react-router-dom';
+import logo from '../assets/logo.png';
 const Navbar = () => {
   return (
-    <div>Navbar</div>
+    <div className="navbar">
+      <div className="container">
+        <div className="logo">
+          <Link to="/">
+            <img src={logo} alt="" />
+          </Link>
+        </div>
+        <div className="links">
+          <Link className="link" to="/?cat=art"><h6>ART</h6></Link>
+          <Link className="link" to="/?cat=science"><h6>SCIENCE</h6></Link>
+          <Link className="link" to="/?cat=technology"><h6>TECHNOLOGY</h6></Link>
+          <Link className="link" to="/?cat=cinema"><h6>CINEMA</h6></Link>
+          <Link className="link" to="/?cat=design"><h6>DESIGN</h6></Link>
+          <Link className="link" to="/?cat=food"><h6>FOOD</h6></Link>
+          <span>UjjalZaman</span>
+          <span>Logout</span>
+
+          <span className="write">
+            <Link to='write' className='link'>Write</Link>
+          </span>
+        </div>
+      </div>
+    </div>
   )
 }
 
